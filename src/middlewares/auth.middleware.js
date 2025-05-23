2b9b00cb7bda984b99b09812909ae758466f76d3
patelshiv86484 <143242758+patelshiv86484@users.noreply.github.com>
2025-05-23 22:04:12 +0530

    Update auth.middleware.js

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,7 +3,7 @@ import {ApiError} from "../utils/ApiError.js"
 import jwt, { decode } from "jsonwebtoken"
 import {User} from "../models/user.model.js"
 //this middleware is designed to add req.user from JWT token(available data for loging out user adn other scenario where user credentials are required but not avialable so take them from jwt token).
-const verifyJWT=asyncHandler(async(req,_,next)=>{//_ inplace of res as of no use over here.
+const verifyJWT=asyncHandler(async(req,res,next)=>{//_ inplace of res as of no use over here.
     
     try {
         // const token=req.cookies?.accessToken || req.header("Authorization");//client request with authorization header. Authorization: Bearer my_jwt_token
@@ -11,7 +11,7 @@ const verifyJWT=asyncHandler(async(req,_,next)=>{//_ inplace of res as of no use
         //     throw new ApiError(401,"Unauthorized reuest")
         // }
           const token = req.cookies.accesstoken
-
+        console.log("TOKEN",token)
         if (!token) return res.status(401).json({ error: 'Token missing' });
         const decodedToken=jwt.verify(token,process.env.ACCESS_TOKEN_SECRET);
         if(!decodedToken){
