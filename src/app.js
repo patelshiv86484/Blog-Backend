b6ad42f2e1a81aed625699c79b6b1ffed02f8190
patelshiv86484 <143242758+patelshiv86484@users.noreply.github.com>
2025-06-25 08:47:44 +0530

    Update app.js for cold start issue

--- src/app.js
+++ src/app.js
@@ -4,10 +4,26 @@ import cookieParser from "cookie-parser"
 const app=express();
 //parsing req.body data using app.use()  ---> middleware function for setting middleware.
 // app.use(cors()) Enable CORS for defined origins.
-app.use(cors({
-    origin:process.env.CORS_ORIGIN,//limits to specific origins only
-    credentials:true,// Allow frontend to send and receive cookies & HTTP authentication
-}))
+// app.use(cors({
+//     origin:process.env.CORS_ORIGIN,//limits to specific origins only
+//     credentials:true,// Allow frontend to send and receive cookies & HTTP authentication
+// }))
+
+//for solving cold start issue
+const allowedOrigin = process.env.CORS_ORIGIN;
+
+app.use(cors((req, callback) => {
+    let corsOptions;
+    if (req.path === '/ping') {
+        corsOptions = { origin: true }; // Allow all origins for /ping
+    } else {
+        corsOptions = {
+            origin: allowedOrigin,
+            credentials: true
+        };
+    }
+    callback(null, corsOptions);
+}));
 
 app.use(express.json({limit:"16kb"}));//parse incoming request bodies as JSON(because When a client sends data to the server, even if it's in JSON format, the payload (the body of the HTTP request) is transmitted as a string.), but with a size limit on the body as 16kb.
 app.use(express.urlencoded({extended:true,limit:"16kb"}));//express.urlencoded() middleware will parse the incoming URL-encoded data, converting it into a JavaScript object that can be easily accessed using req.body.
